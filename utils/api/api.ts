ee161165b958ba48838081ef41815ae1772e7ab9
IST210Group11 <kappalordu@gmail.com>
2021-10-31 00:30:26 -0400

    api

--- utils/api/api.ts
+++ utils/api/api.ts
@@ -2,7 +2,7 @@ import DailyValues from "../../entities/DailyValues";
 import finaltable from "../../entities/finaltable";
 import FoodList from '../../entities/FoodList';
 import Cors from 'cors';
-import {Connection, createConnection, getConnection, getConnectionOptions, getRepository} from "typeorm";
+import {Connection, createConnection, getConnection, getConnectionOptions, getRepository, getConnectionManager} from "typeorm";
 
 const initMiddleware = (middleware) => (req, res) => new Promise((resolve, reject) => {
     middleware(req, res, (result) => {
@@ -15,17 +15,25 @@ export const cors = initMiddleware(Cors({ origin: "*", methods: ["GET", "POST", 
 
 export const connect = async () => {
     try {
-        const connection = getConnection()
+        const connection = getConnectionManager().get("default");
         await connection.close()
     } catch (e) {
         console.error(e)
     }
-    const connectionOptions = await getConnectionOptions()
-    const options: any = {
-        ...connectionOptions,
-        entities: [DailyValues, finaltable, FoodList]
-    }
-    return await createConnection(options)
+    const connection = await createConnection({
+        name: "default",
+        type: "sqlite",
+        entities: [DailyValues, finaltable, FoodList],
+        database: "db.db",
+        synchronize: true
+    })
+    // const connectionOptions = await getConnectionOptions()
+    // const options: any = {
+    //     ...connectionOptions,
+    //     entities: [DailyValues, finaltable, FoodList]
+    // }
+    // return await createConnection(options)
+    return connection
 }
 
 export const getRepo = (connection: Connection, repo: "DailyValues" | "FDC" | "FoodList") => {
