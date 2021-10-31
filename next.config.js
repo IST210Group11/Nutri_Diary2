64f0bb72d816f2832cc3dcc0b883811913e7b212
IST210Group11 <kappalordu@gmail.com>
2021-10-31 16:39:38 -0400

    test10

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "https://nutri-diary2.vercel.app/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
