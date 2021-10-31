26ac85f52539295ef8fd966272f54716b15434f2
IST210Group11 <kappalordu@gmail.com>
2021-10-31 13:05:19 -0400

    test2

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,11 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    development: {
+      client: 'sqlite3',
+      connection: {
+      filename: 'db.db'
+      }
+    }
   },
 }
