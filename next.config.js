5237050f25363e93dc4aaf31f72aaef90adcb48c
IST210Group11 <kappalordu@gmail.com>
2021-10-31 13:39:27 -0400

    test3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    BASE_URL: "https://nutri-diary2.vercel.app"
+    BASE_URL: "https://nutri-diary-capstone.herokuapp.com"
     // BASE_URL: "http://localhost:3000"
   }
 }
